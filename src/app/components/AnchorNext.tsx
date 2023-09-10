ae999a3805b6c76ebaceb239a8a50116cca0bca4
nobuki <nobking.63@gmail.com>
2023-09-11 08:19:07 +0900

    arrow changed

--- src/app/components/AnchorNext.tsx
+++ src/app/components/AnchorNext.tsx
@@ -12,7 +12,7 @@ const AnchorNext = (props: Props) => {
     
   return (
       <div className='flex items-center justify-end mt-[20px] md:mt-[30px] mb-[300px] sticky bottom-0 left-0 right-0'>
-        <Scroll to={`${nextTarget}`} className="relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 text-right block" aria-label="次のスライドへ" smooth={true}>&gt;</Scroll>
+        <Scroll to={`${nextTarget}`} className="arrow-right relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 block bg-[rgba(255,255,255,0.1)] backdrop-blur-sm w-[30px] md:w-[45px] h-[30px] md:h-[45px] text-center rounded-full" aria-label="次のスライドへ" smooth={true}></Scroll>
       </div>
   )
 }
--- src/app/components/AnchorPrevTop.tsx
+++ src/app/components/AnchorPrevTop.tsx
@@ -13,8 +13,8 @@ const AnchorPrevTop = (props: Props) => {
     
   return (
       <div className='flex items-center justify-between mt-[20px] md:mt-[30px] mb-[300px] sticky bottom-0 left-0 right-0'>
-        <Scroll to={`${prevTarget}`} className="relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 text-right block" aria-label="前のスライドへ" smooth={true}>&lt;</Scroll>
-        <Scroll to={`${mvTarget}`} className="relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 text-right block" aria-label="トップへ戻る" smooth={true}>&gt;&gt;</Scroll>
+        <Scroll to={`${prevTarget}`} className="arrow-left relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 block bg-[rgba(255,255,255,0.1)] backdrop-blur-sm w-[30px] md:w-[45px] h-[30px] md:h-[45px] text-center rounded-full" aria-label="前のスライドへ" smooth={true}></Scroll>
+        <Scroll to={`${mvTarget}`} className="arrow-top relative cursor-pointer text-[20px] md:text-[30px] font-bold transition-opacity hover:opacity-70 block bg-[rgba(255,255,255,0.1)] backdrop-blur-sm w-[30px] md:w-[45px] h-[30px] md:h-[45px] text-center rounded-full" aria-label="トップへ戻る" smooth={true}></Scroll>
       </div>
   )
 }
